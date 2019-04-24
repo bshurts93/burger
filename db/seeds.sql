a9e497c0f98e116db201f28c1d6e9e437ac56377
Ben Shurts <bshurts93@gmail.com>
2019-04-24 14:05:43 -0500

    Using postman, successfully able to add items to the DB using a post request with request body information

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Elkstravagent Burger", false),
+    ("The Krusty Burger", true),
+    ("Ramen Burger", false),
+    ("The Quadruple Bypass Burger", false),
+    ("The Impossible Burger", false);
+
