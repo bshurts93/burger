6bc7c8aefc92a64cffb495e8027f1ecd4795d9de
Ben Shurts <bshurts93@gmail.com>
2019-04-25 13:04:41 -0500

    Added form to submit new burger, adds to DB and reloads page to show new addition

--- public/assets/js/burger_logic.js
+++ public/assets/js/burger_logic.js
@@ -8,36 +8,33 @@ $(function () {
             devoured: newState
         };
 
-        // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newDevouredState
         }).then(
             function () {
                 console.log("changed sleep to", newDevouredState);
-                // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
 
     $(".create-form").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
-        var newCat = {
-            name: $("#ca").val().trim(),
-            sleepy: $("[name=sleepy]:checked").val().trim()
+        var newBurger = {
+            burger_name: $("#name-input").val().trim(),
+            devoured: $("[name=devoured-check]:checked").val().trim()
         };
 
-        // Send the POST request.
-        $.ajax("/api/cats", {
+        console.log(newBurger);
+
+        $.ajax("/api/burgers", {
             type: "POST",
-            data: newCat
+            data: newBurger
         }).then(
             function () {
-                console.log("created new cat");
-                // Reload the page to get the updated list
+                console.log("New burger created!");
                 location.reload();
             }
         );
